cb3a7d0168fb21e0d0dc111d265ce5efbc1b91ca
josh-ten <joshuatencic@gmail.com>
2018-11-26 14:28:31 +1100

    a

--- projectLoader.js
+++ projectLoader.js
@@ -1,17 +1,19 @@
-$.getJSON('./website/projectRef.json', (data) => { 
-    let url = window.location.href;
-    let projectId = url.split('#')[1];
-    // console.log(projectId);
-    let project = findDataForId(data, projectId);
-    let gitHubName = (project.gitHubName == null) ? 
-        project.title.replace(/\s/g, '') : project.gitHubName;
-    let hostBaseUrl = "https://cdn.jsdelivr.net/gh/josh-ten/";
-    for (let fileName of project.files) {
-        let fileUrl = hostBaseUrl + gitHubName + '/' + fileName;
-        console.log(fileUrl);
-        $.getScript(fileUrl);
-    }
-});
+// $(document).ready(() => {
+    $.getJSON('./website/projectRef.json', (data) => { 
+        let url = window.location.href;
+        let projectId = url.split('#')[1];
+        // console.log(projectId);
+        let project = findDataForId(data, projectId);
+        let gitHubName = (project.gitHubName == null) ? 
+            project.title.replace(/\s/g, '') : project.gitHubName;
+        let hostBaseUrl = "https://cdn.jsdelivr.net/gh/josh-ten/";
+        for (let fileName of project.files) {
+            let fileUrl = hostBaseUrl + gitHubName + '/' + fileName;
+            console.log(fileUrl);
+            $.getScript(fileUrl);
+        }
+    });
+// });
 
 function findDataForId(data, id) {
     let projectData;
