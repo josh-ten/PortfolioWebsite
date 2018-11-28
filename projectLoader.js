cd22b138d884c0bfdbc1d321572aaea705e36c81
josh-ten <joshuatencic@gmail.com>
2018-11-28 14:39:52 +1100

    load p5 after the fact

--- projectLoader.js
+++ projectLoader.js
@@ -1,19 +1,22 @@
-// $(document).ready(() => {
-    $.getJSON('./website/projectRef.json', (data) => { 
-        let url = window.location.href;
-        let projectId = url.split('#')[1];
-        // console.log(projectId);
-        let project = findDataForId(data, projectId);
-        let gitHubName = (project.gitHubName == null) ? 
-            project.title.replace(/\s/g, '') : project.gitHubName;
-        let hostBaseUrl = "https://cdn.jsdelivr.net/gh/josh-ten/";
-        for (let fileName of project.files) {
-            let fileUrl = hostBaseUrl + gitHubName + '/' + fileName;
-            console.log(fileUrl);
-            $.getScript(fileUrl);
-        }
-    });
-// });
+$.getJSON('./website/projectRef.json', (data) => { 
+    let url = window.location.href;
+    let projectId = url.split('#')[1];
+    // console.log(projectId);
+    let project = findDataForId(data, projectId);
+    let gitHubName = (project.gitHubName == null) ? 
+        project.title.replace(/\s/g, '') : project.gitHubName;
+    let hostBaseUrl = "https://cdn.jsdelivr.net/gh/josh-ten/";
+    let filesLoaded = 0;
+    for (let fileName of project.files) {
+        let fileUrl = hostBaseUrl + gitHubName + '/' + fileName;
+        console.log(fileUrl);
+        $.getScript(fileUrl, () => {
+            filesLoaded++;
+            if (filesLoaded === project.files.length)
+                $.getScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js');
+        });
+    }
+});
 
 function findDataForId(data, id) {
     let projectData;
