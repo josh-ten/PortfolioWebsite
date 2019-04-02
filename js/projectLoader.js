b010d574565900c0133ee0e5555fc622ac39dc60
rmit-s3657249-joshua-tencic <s3657249@student.rmit.edu.au>
2019-04-02 22:38:45 +1100

    changed loading order of jQuery and project files

--- js/projectLoader.js
+++ js/projectLoader.js
@@ -1,32 +1,19 @@
-$.getJSON('./projectRef.json', (data) => { 
+$.getJSON('./projectRef.json', data => { 
     let url = window.location.href;
     let projectId = url.split('#')[1];
-    // console.log(projectId);
     let project = findDataForId(data, projectId);
     let gitHubName = (project.gitHubName == null) ? 
         project.title.replace(/\s/g, '') : project.gitHubName;
     let hostBaseUrl = "https://cdn.jsdelivr.net/gh/josh-ten/";
-    let filesLoaded = 0;
+    $.getScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js');
     for (let fileName of project.files) {
         let fileUrl = hostBaseUrl + gitHubName + '/' + fileName;
-        console.log(fileUrl);
-        $.getScript(fileUrl, () => {
-            filesLoaded++;
-            if (filesLoaded === project.files.length)
-                $.getScript('https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.2/p5.min.js');
-        });
+        $.getScript(fileUrl);
     }
 });
 
 function findDataForId(data, id) {
-    let projectData;
-    for (let project of data) {
-        if (project.id == id) {
-            projectData = project;
-            break;
-        }
-    }
-    return projectData;
+    return data.find((project) => { return project.id == id; });
 }
 
 function attachCanvasToContainer() {
